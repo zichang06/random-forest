11bba72fb0aff268be1c009c4f238f8486db0adf
zichang <caoruixunzhao2006@126.com>
2018-07-11 17:18:00 +0800

    update

---
+++ myRF_v4/backup/myForest.py
@@ -0,0 +1,328 @@
+import csv
+from collections import defaultdict
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn import preprocessing
+from sklearn import tree
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
+from collections import OrderedDict
+import multiprocessing
+from multiprocessing import Pool, Manager
+import math
+import random
+import threading
+import time
+import sys
+
+from myTree import myTree
+record = []
+
+#!!! 样本不均衡，导致容易误判成负样本，增加对数变换y = log2(x + 1)
+# 或许解决的更好方法，是实现计算出所有正负样本之比，在结点投票时，把弱势样本乘以一个比例
+
+trainSampleNum = int(50/2)
+testSampleNum = 20
+train_dir = "simple_data/train.txt"
+test_dir = "simple_data/test.txt"
+treeNum = 100
+maxDepth = 5
+
+trainSampleNum = int(1719692 / 1000)
+testSampleNum = 429923
+train_dir = "data/train.txt"
+test_dir = "data/test.txt"
+treeNum = 100
+maxDepth = 5
+
+preDir = "test.csv"
+maxSplitNum = 10
+featureNum = 201
+
+def writeCSV(predictLable, fileName = "pre.csv"):
+    print(">> writing to csv %s..." %fileName)
+    names = range(testSampleNum)
+    dataframe = pd.DataFrame({'id':names,'label':predictLable})
+    dataframe.to_csv(fileName,index=False,sep=',', encoding = "utf-8")
+
+def loadTrainingData(dataDir = "train.txt"):
+    print(">> start loading data %s..." %(dataDir))
+    '''
+    readin dataset according to dataDir
+    if train dataset is needed
+    return augmented trainingData[m, n+1], the last colomn is label(1 or 0)
+    if test dataset is needed
+    return augmented testData[m, n], without label
+
+    '''
+    label = []
+    features = [{0: {1: [], 0:[]}} for n in range(featureNum)]
+    f = open(dataDir)
+    lines = f.readline()
+    sampleCount = 0
+    while lines:
+        if sampleCount == trainSampleNum:
+            break
+        line = lines.split(' ')
+        for index in range(len(line)):
+            if index == 0:
+                label.append(int(line[0]))
+                continue
+            colon = line[index].index(':')
+            r = float(line[index][colon+1:])  # 该样本的该特征值
+            i = int(line[index][:colon]) - 1  # 哪个特征
+            if r not in features[i]:
+                features[i][r] = {1: [], 0:[]}
+            if label[sampleCount] == 1:
+                features[i][r][1].append(sampleCount)
+            if label[sampleCount] == 0:
+                features[i][r][0].append(sampleCount)
+        lines = f.readline()
+        sampleCount += 1
+        sys.stdout.write('\r>> %d ' % (sampleCount))
+
+    for item in features:
+        item.pop(0)
+    f.close()
+    print("\n>> finish load data %s..." %(dataDir))
+
+    return features, label
+
+def sortFeatures(features, splitNum):
+    '''
+    找出特征的若干分割点，并从小到大排序
+    '''
+    sortedFeatures = []
+    featureCount = 0
+    # !!!选取分裂结点时，需要判断一下
+    # sortedFeatures列表的下标表示哪一个特征，如果第i个特征不存在数据，那么len(sortedFeatures[i]) == 1,只有第一个
+    for feature in features: # 遍历每个特征 O(m)  这个嵌套循环大概走2000次
+        sortedFeatures.append({0:{1: [], 0:[]}})  # 遍历每个
+        if len(feature.keys()) == 0:
+            featureCount+=1
+            continue
+        splitNum = min(splitNum, len(feature.keys()))
+        maxValue = max(feature.keys())
+        minValue = min(feature.keys())
+        adder = (maxValue - minValue) / splitNum
+        for i in range(splitNum):  # 遍历每个分裂点(k'≈10)
+            splitPoint = float(minValue + (i + 0.5) * adder)
+            sortedFeatures[featureCount][splitPoint] = {1: [], 0:[]}
+        featureCount+=1
+        sys.stdout.write('\r>> finish spilting %dst feature ' % (featureCount))
+    print("\nbegin to arrange split points for features...")
+    del featureCount
+
+
+    for featureCount in range(len(sortedFeatures)):
+        splitValues = list(sortedFeatures[featureCount])
+        for oriPoint in features[featureCount]:
+
+            diff = [abs(oriPoint - splitValue) for splitValue in splitValues]
+            nearestSplitValue = splitValues[diff.index(min(diff))]
+            sortedFeatures[featureCount][nearestSplitValue][1] += features[featureCount][oriPoint][1]
+            sortedFeatures[featureCount][nearestSplitValue][0] += features[featureCount][oriPoint][0]
+            sys.stdout.write('\r>> finish arranging %dst feature ' % (featureCount+1))
+
+    print("\n>>finish sorting Features.")
+    return sortedFeatures
+
+def loadTestData(dataDir = "test.txt"):
+    print(">> start loading data %s..." %(dataDir))
+    '''
+    readin dataset according to dataDir
+    if test dataset is needed
+    return augmented testData[m, n], without label
+    可能要对test的缺失值进行补零
+
+    '''
+    data = np.zeros((testSampleNum, featureNum))
+    f = open(dataDir)
+    lines = f.readline()
+    sampleCount = 0
+    while lines:
+        line = lines.split(' ')
+        for index in range(len(line)):
+            if index == 0:
+                continue
+            colon = line[index].index(':')
+            data[sampleCount, int(line[index][:colon]) - 1] = float(line[index][colon+1:])
+        lines = f.readline()
+        sampleCount += 1
+        sys.stdout.write('\r>> %d ' % (sampleCount))
+    f.close()
+    print("\n>> finish load data %s..." %(dataDir))
+
+    return data
+
+class myForest:
+    def __init__(self, n_bootstrapSamples=10, maxDepth = 10):
+        self.n_bootstrapSamples = n_bootstrapSamples
+        self.list_tree = []
+        self.list_featureIndex= []
+        self.maxDepth = maxDepth
+
+    def generateBootstrapSamples(self):
+        '''
+        构造bootstrap样本，随机选取50%的样本，并随机选取70%的特征
+        返回两个list，rows和columns
+        '''
+        k = int(0.7 * featureNum)
+        columns = random.sample(range(featureNum), k)
+
+        rowRange = np.arange(trainSampleNum)
+        np.random.shuffle(rowRange)
+        halfSampleNum = int(trainSampleNum / 2)
+        rows = rowRange[:halfSampleNum]
+
+        return rows, columns
+
+    def buildTree(self, index):
+        '''
+        构建一棵树
+        '''
+        # 返回两个list
+        rows, columns = self.generateBootstrapSamples()
+        print("building the %dst tree..." %(index + 1))
+        currentTree = myTree(maxLevel = self.maxDepth)
+        currentTree.fit(label = self.label, sortedFeatures = self.sortedFeatures, rows = rows, columns = columns, treeID = index)
+
+        self.list_tree.append(currentTree)
+        self.list_featureIndex.append(columns)
+
+        print("finish building the %dst tree..." %(index + 1))
+        return currentTree
+
+    def fit(self, sortedFeatures, label):
+        '''
+        构造随机森林,不是多线程
+        '''
+        self.label = label
+        self.sortedFeatures = sortedFeatures
+
+        for i in range(self.n_bootstrapSamples):
+            self.buildTree(i)
+
+    def fitWithMultiProcess(self, sortedFeatures, label):
+        '''
+        多线程构造随机森林
+        '''
+        self.label = label
+        self.sortedFeatures = sortedFeatures
+
+        #mylist=Manager().list()
+
+        p = multiprocessing.Pool(processes=5)
+        for i in range(self.n_bootstrapSamples):
+             p.apply_async(self.buildTree,args=(i,))
+            #self.list_tree.append(result.get())
+        p.close()
+        p.join()
+
+        # for i in range(self.n_bootstrapSamples):
+        #     process = multiprocessing.Process(target=self.buildTree,args=(i,))
+        #     process.start()
+        #     record.append(process)
+
+        # for process in record:
+        #     process.join()
+
+        # record.clear()
+
+        print("finish building forest tree, main thread go on...")
+
+    def treePredict(self, results, i):
+        '''
+        一棵树预测
+        '''
+        print("predicting for the %dst tree..." %(i + 1))
+        tmp = self.list_tree[i].predict(self.data)
+        results[:, i] = tmp
+        print("finish predicting for the %dst tree..." %(i + 1))
+        return tmp
+
+    def predict(self, data):
+        '''
+        利用随机森林对给定观测数据进行分类
+        '''
+        print("predicting...")
+        self.data = data
+        results = np.zeros((len(self.data), self.n_bootstrapSamples))
+
+        for i in range(len(self.list_tree)):
+            self.treePredict(results, i)
+
+        #threshold = np.full(len(self.data), 0.5)
+        tmp = np.mean(results, axis=1) # 计算每一行的均值
+        #!!! 样本不均衡，导致容易误判成负样本，增加对数变换y = log2(x + 1)
+        finalResult = [math.log(x + 1, 2) for x in tmp]
+        #finalResult = np.greater(tmp, threshold)
+        return finalResult
+
+    def predictWithMultiProcess(self, data):
+        '''
+        多线程利用随机森林对给定观测数据进行分类
+        '''
+        self.data = data
+
+        print("predicting...")
+        results = np.zeros((len(self.data), self.n_bootstrapSamples))
+
+        p = multiprocessing.Pool(processes=10)
+        for i in range(len(self.list_tree)):
+            result = p.apply_async(self.treePredict,args=(results, i))
+            results[:, i] = result.get()
+        p.close()
+        p.join()
+
+        # for i in range(len(self.list_tree)):
+        #     process = multiprocessing.Process(target=self.treePredict,args=(results, i))
+        #     process.start()
+        #     record.append(process)
+
+        # for process in record:
+        #     process.join()
+
+        # record.clear()
+
+        #threshold = np.full(len(self.data), 0.5)
+        tmp = np.mean(results, axis=1) # 计算每一行的均值
+        #!!! 样本不均衡，导致容易误判成负样本，增加对数变换y = log2(x + 1)
+        finalResult = [math.log(x + 1, 2) for x in tmp]
+        #finalResult = np.greater(tmp, threshold)
+        return finalResult
+
+if __name__ == '__main__':
+    t0 = time.time()
+    print('>> my Forest with %d trees(depth: %d)' %(treeNum, maxDepth))
+
+    features, label = loadTrainingData(train_dir)
+    t1 = time.time()
+    print('>> load training data time %.2fs.' %(float(t1 - t0)))
+    sortedFeatures = sortFeatures(features, maxSplitNum)
+    del features
+    t2 = time.time()
+    print('>> load training data time %.2fs.' %(float(t2 - t1)))
+
+    #clf = RandomForestClassifier(10)
+    clf = myForest(treeNum, maxDepth)
+    print(">> fitting...")
+    #clf.fit(sortedFeatures, label)
+    clf.fitWithMultiProcess(sortedFeatures, label)
+    t3 = time.time()
+    print('>> fit time %.2fs.' %(float(t3 - t2)))
+
+
+    data = loadTestData(test_dir)
+    t4 = time.time()
+    print('>> load test data time %.2fs.' %(float(t4 - t3)))
+
+    print(">> predicting...")
+    pre = clf.predict(data)
+    #pre = clf.predictWithMultiProcess(data)
+    #pre = pre.astype(int)
+    t5 = time.time()
+    print('>> predict time %.2fs.' %(float(t5 - t4)))
+
+    writeCSV(pre, preDir)
---
+++ myRF_v4/backup/myTree.py
@@ -0,0 +1,228 @@
+import csv
+from collections import defaultdict
+import pandas as pd  
+import numpy as np
+import sklearn
+from sklearn import preprocessing
+from sklearn import tree
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
+import math
+import random  
+import threading
+import time
+import sys
+# 构造函数、fit、predict的cols和rows的处理
+# count sys.stdout.write,这两个关键词是用来检测进度的
+
+lock=threading.Lock()
+
+class node:
+    def __init__(self, col=-1, value=None, results=None, trueBranch=None, falseBranch=None):
+        self.col = col  # 分裂的是哪个特征
+        self.value = value  # 分裂的这个特征值为何值
+        self.results = results  # 本叶子结点的数据
+        self.trueBranch = trueBranch  # 指向左子树
+        self.falseBranch = falseBranch  # 指向右子树
+        
+    def getLabel(self):
+        '''
+        选取出本结点的主要类别
+        如果非叶子结点，则返回None
+        '''
+        if self.results == None:
+            return None
+        else:
+            max_counts = 0
+            for key in self.results.keys():
+                if self.results[key] > max_counts:
+                    label = key
+                    max_counts = self.results[key]
+        return label
+
+# Important part
+class myTree:
+    def __init__(self, data=None, label = None, maxLevel=10, rows = None, columns = None):
+        self.data = data  #本树的data
+        self.label = label  # 本树的label
+        self.maxLevel = maxLevel  # 该树最大深度为多少
+        self.rows = rows  # 构建本树所运用的样本下标，一维数组
+        self.columns = columns  # 本树所分得特征对应的下标，一维数组 
+        #data[sample[i]][columns[j]] 表示本棵树的第i个样本，第j个特征
+        self.root = None  # 本树的根节点，即一个node
+
+    def uniqueCounts(self, Trows, Frows):
+        '''
+        计算该结点每个类别各有多少样本
+        param rows: 该结点拥有data的哪些samples
+        return: result是dict，label为Key, 对应的值为该结点下某label有多少个样本
+        '''
+        results = {1:len(Trows), 0:len(Frows)}
+        return results
+
+    def giniEstimate(self, Trows, Frows):
+        '''
+        计算该结点的基尼系数
+        param rows: 该结点拥有data的哪些samples
+        参考：https://www.cnblogs.com/pinard/p/6053344.html
+        运用的是该参考博客的第一个公式
+        '''
+        total = len(Trows) + len(Frows)
+        if total == 0: return 0
+        gini = pow(len(Trows), 2) + pow(len(Frows), 2)
+        gini = 1 - gini / pow(total, 2)
+        return gini
+    
+    def divideSet(self, Trows, Frows, column, value):
+        '''
+        划分子集，这里默认特征值都是连续值
+        大的放在右子树1 trueBranch
+        小的放在左子树2 frueBranch
+        param rows: 该结点拥有data的哪些samples
+        param column: 哪一个特征，这里传入的是真实的特征下标
+        '''
+        Trows1 = []
+        Frows1 = []
+        Trows2 = [] 
+        Frows2 = []
+
+        # lambda???
+        for item in self.sortedFeatures[column].keys():
+            if  item >= value:  #!!!不确定这样是否对，item是社么样
+                Trows1 += self.sortedFeatures[column][item][1]
+                Frows1 += self.sortedFeatures[column][item][0]
+            else:
+                Trows2 += self.sortedFeatures[column][item][1]
+                Frows2 += self.sortedFeatures[column][item][0]
+
+        Trows1 = list(set(Trows1).intersection(set(Trows)))
+        Frows1 = list(set(Frows1).intersection(set(Frows)))
+        Trows2 = list(set(Trows2).intersection(set(Trows)))
+        Frows2 = list(set(Frows2).intersection(set(Frows)))
+
+        subTrows = Trows1 + Trows2
+        subFrows = Frows1 + Frows2
+
+        # splitFunction = lambda row: self.data[row][column] >= value 
+        # rows1 = [row for row in rows if splitFunction(row)]
+        # rows2 = [row for row in rows if not splitFunction(row)]
+        return (Trows1, Frows1, Trows2, Frows2, subTrows, subFrows)
+
+    def buildTree(self, Trows = [], Frows = [], level = 0):
+        '''
+        构造CART决策树
+        param rows: 该结点拥有data的哪些samples
+        param columns: 对应的特征下标
+        '''
+        sys.stdout.write("--> now in depth %d. of %s st tree \n" %(level, self.treeID))
+        if len(Trows) + len(Frows) == 0:
+            return node()
+        if level > self.maxLevel:
+            return node(results=self.uniqueCounts(Trows, Frows))
+
+        bestGain = 0
+        bestCriteria = None
+        bestSets = None
+
+        colCount = len(self.columns)   # 特征数
+        colRange = np.arange(colCount)  # colRange数组存储特征下标。对某一行的样本来说，
+                                        # sample[colRange[j]],就是取该样本第j个特征，colRange数组中的下标
+        np.random.shuffle(colRange)  # 乱序特征，存储的是下标, 随机选取特征总数开方的样本进行分裂
+        countCol = 0
+        #total = int(math.ceil(colCount / 4)) 
+        total = int(math.ceil(math.sqrt(colCount)))  # 如果样本太少，可能找到的可行分裂特征会很少甚至没有
+        for col in colRange[0:total]:  # self.columns[col]是原来真正的哪一个特征
+            countCol+=1
+            #sys.stdout.write("--> -- the %dst feature in total of %d. \n" %(countCol, total))
+            colValues = self.sortedFeatures[self.columns[col]]  # dict，对于该特征，所有样本共多少种取值
+            count = 0
+            for value in colValues.keys():  # 寻找分裂点
+                count += 1
+                #sys.stdout.write("--> find the %d st split point of %d in total. \n" %(count, len(colValues.keys())))
+                if count == 1 or count == len(colValues):
+                    continue  # 取第一个值和最后一个值做分裂点毫无意义
+                #!!!这里有个细节，对某个特征而言，分裂前后的rows不再相同,比较基尼系数，前面的应该用子集
+                (Trows1, Frows1, Trows2, Frows2, subTrows, subFrows) = self.divideSet(Trows, Frows, self.columns[col], value)
+                currentGini = self.giniEstimate(subTrows, subFrows)
+                tmp =  (len(Trows1) + len(Frows1)) * self.giniEstimate(Trows1, Frows1) + (len(Trows2) + len(Frows2)) * self.giniEstimate(Trows2, Frows2)
+                if len(subTrows) + len(subFrows) == 0:  #!!!存在分母为0的情况，这时该分裂点无参考价值
+                    continue
+                gain = currentGini - tmp / (len(subTrows) + len(subFrows))
+                if gain > bestGain and len(Trows1) + len(Frows1) > 0 and len(Trows2) + len(Frows2) > 0:
+                    bestGain = gain
+                    bestCriteria = (self.columns[col], value)
+                    bestSets = (Trows1, Frows1, Trows2, Frows2)
+        if bestGain > 0:
+            trueBranch = self.buildTree(bestSets[0], bestSets[1], level = level+1)
+            falseBranch = self.buildTree(bestSets[2], bestSets[3], level = level+1)
+            return node(col=bestCriteria[0],value=bestCriteria[1], trueBranch=trueBranch, falseBranch=falseBranch)
+        else:
+            return node(results=self.uniqueCounts(Trows, Frows))
+
+    def fit(self, label, rows, columns, sortedFeatures, treeID = 0):
+        '''
+        初始化，并构造一棵树
+        '''
+        self.label = label
+        self.rows = rows
+        self.columns = columns
+        self.sortedFeatures = sortedFeatures
+        self.treeID = treeID
+
+        Trows = []
+        Frows = []
+        for i in rows:  # 遍历每一个样本  lambda???
+            if self.label[i] == 1:
+                Trows.append(i)
+            if self.label[i] == 0:
+                Frows.append(i)
+
+        self.root = self.buildTree(Trows, Frows, level = 0)
+        self.printTree(self.root)
+    
+    def printTree(self, node, indent='    '):  
+        '''
+        以文本形式显示决策树
+        调用：clf.printTree(clf.tree)
+        '''
+        #lock.acquire()
+        if node.results != None:
+            print(str(node.results))
+        else:
+            print('F'+ str(node.col)+' >='+str(node.value)+'?  ')
+            print(indent+'T->  ', end=""),
+            self.printTree(node.trueBranch, indent + '    ')
+            print(indent+'F->  ', end=""),
+            self.printTree(node.falseBranch, indent + '    ')
+        #lock.release()
+
+    def predict_one_sample(self, node, row):
+        '''
+        利用决策树进行分类
+        param node: 该
+        param row: 该行数据
+        '''
+        if node.results != None:
+            return node.getLabel()
+        else:
+            v = row[node.col]
+            branch = None
+            if v >= node.value: 
+                branch = node.trueBranch
+            else: 
+                branch = node.falseBranch
+            return self.predict_one_sample(branch, row)
+
+    def predict(self, data):
+        '''
+        利用决策树进行分类
+        '''
+        self.data = data
+        self.rows = range(len(data))
+
+        finalResults = []
+        for row in self.data:
+            finalResult = self.predict_one_sample(self.root, row)
+            finalResults.append(finalResult)
+
+        return finalResults
